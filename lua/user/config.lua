3f0d7a2f08e76612c074c8b848971d5f41ec7b9d
Alex Campos <alex.campos@gmail.com>
2022-05-06 12:11:15 -0500

    updated settings from windows

--- lua/user/config.lua
+++ lua/user/config.lua
@@ -7,3 +7,17 @@ parser_config.powershell = {
   filetype = "ps1",
   used_by = { "psm1", "psd1", "pssc", "psxml", "cdxml" }
 }
+
+
+-- Lua:
+-- For dark theme
+--vim.g.vscode_style = "dark"
+-- For light theme
+-- vim.g.vscode_style = "light"
+-- Enable transparent background
+--vim.g.vscode_transparent = 1
+-- Enable italic comment
+--vim.g.vscode_italic_comment = 1
+-- Disable nvim-tree background color
+--vim.g.vscode_disable_nvimtree_bg = true
+----vim.cmd([[colorscheme vscode]])
